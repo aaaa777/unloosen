f30dec5a1d8abae20edf5f243552c267f9ce4289
aaaa777(hiupc) <aaaa777@users.noreply.github.com>
2023-05-07 17:58:13 +0900

    [fix] wrong match

--- lib/unloosen/mode/sandbox.rb
+++ lib/unloosen/mode/sandbox.rb
@@ -3,7 +3,7 @@ require_relative "./mode"
 module Unloosen::Mode
     class Sandbox < BaseMode
         def should_load?
-            ::Unloosen::CURRENT_EVENT != :sandbox && super
+            ::Unloosen::CURRENT_EVENT == :sandbox && super
         end
     end
 end
