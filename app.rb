fb9053cfef43c6142bf09452f9327df9d2a3b809
aaaa777(hiupc) <aaaa777@users.noreply.github.com>
2023-05-07 19:14:22 +0900

    [add] popup example

--- app.rb
+++ app.rb
@@ -34,4 +34,18 @@ background do
     
 end
 
+popup do
+    main_div = document.getElementsByClassName('omikuji')[0]
+    btn = document.createElement('button')
+    btn.innerText = 'draw omikuji'
+    res = document.createElement('h2')
+
+    btn.addEventListener('click') do |e|
+        res.innerText = ['lucky', 'unlucky'].sample
+    end
+    
+    main_div.innerText = ''
+    main_div.appendChild(res)
+    main_div.appendChild(btn)
+end
 
