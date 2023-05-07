ec650731dcb5fc0adb19acfcd5a73be150e3d3ea
aaaa777(hiupc) <aaaa777@users.noreply.github.com>
2023-05-08 06:54:21 +0900

    [fix] require_relative in remote

--- lib/unloosen/utils/require_remote.rb
+++ lib/unloosen/utils/require_remote.rb
@@ -26,19 +26,42 @@ class << self
         return super lib if @base_uris&.empty?
 
         # save curretn uri
-        last_pwd_uri = @pwd_uri
+        _pwd_path, _pwd_uri, _pwd_base_uri = @pwd_path, @pwd_uri, @pwd_base_uri
 
-        return true if @base_uris&.find do |base_uri|
+        # set pwd_uri
+        @pwd_path = lib.delete_prefix('/')
+
+        # require_relative
+        if @remote_relative then
+            @remote_relative = false
+            lib_uri = @pwd_base_uri + @pwd_path
+            src = fetch_remote_text(lib_uri)
+            
+            return false unless src
+            
+            @pwd_uri = lib_uri
+            @pwd_path = lib
+
+            Kernel.eval(
+                src,
+                TOPLEVEL_BINDING,
+                lib_uri.to_s
+            )
+            
+            @pwd_path = _pwd_path
+            return true
+        end
+
+        # require
+        @base_uris&.find do |base_uri|
             lib_uri = base_uri + lib
             return false if @loaded_uris.include?(lib_uri.to_s)
  
             src = fetch_remote_text(lib_uri)
-
             next false unless src
             
-            # set pwd_uri
             @pwd_uri = lib_uri
-            
+            @pwd_base_uri = base_uri
             # eval lib src
             Kernel.eval(
                 src,
@@ -50,56 +73,66 @@ class << self
             @loaded_uris << lib_uri.to_s
 
             # set this pwd_uri scope
-            @pwd_uri = last_pwd_uri
-            true
+            @pwd_path, @pwd_uri, @pwd_base_uri = _pwd_path, _pwd_uri, _pwd_base_uri
+            return true
         end
         
         # when libs not found in remote
-        super lib
+        @pwd_path, @pwd_uri, @pwd_base_uri = nil
+        res = super lib
+        @pwd_path, @pwd_uri, @pwd_base_uri = _pwd_path, _pwd_uri, _pwd_base_uri
+        res
     end
 
     def require_relative(lib)
 
-        relative_from = caller_locations(1..1).first
-        relative_from_path = relative_from.absolute_path || relative_from.path
-        absolute_lib = File.expand_path("../#{lib}", relative_from_path)
-
-        return self.require absolute_lib unless @pwd_uri
-        
         # what a...
         if lib == "require_test_library" then
             return "test_lib!"
         end
+
+        unless @pwd_base_uri then
+            relative_from = caller_locations(1..1).first
+            relative_from_path = relative_from.absolute_path || relative_from.path
+            absolute_lib = File.expand_path("../#{lib}", relative_from_path)
+
+            self.require absolute_lib
+        end
+        # save current uri
+        _pwd_path, _pwd_uri, _pwd_base_uri = @pwd_path, @pwd_uri, @pwd_base_uri
+
+        @pwd_path = File.expand_path("../#{lib}", @pwd_path).delete_prefix('/')
+        @remote_relative = true
+
+        #lib_uri = @pwd_base_uri + @pwd_path
+        #return false if @loaded_uris&.include?(lib_uri.to_s)
+
+        res = self.require(@pwd_path)
+        @pwd_path, @pwd_uri, @pwd_base_uri = _pwd_path, _pwd_uri, _pwd_base_uri
+        res
         
-        #return super lib if @base_uris.empty?
 
-        # save curretn uri
-        last_pwd_uri = @pwd_uri
+        # src = fetch_remote_text(lib_uri)
 
-        lib_uri = @pwd_uri + lib
-        return false if @loaded_uris&.include?(lib_uri.to_s)
-
-        src = fetch_remote_text(lib_uri)
-
-        # when libs not found in remote
-        return self.require absolute_lib unless src
+        # # when libs not found in remote
+        # return self.require absolute_lib unless src
         
-        # set pwd_uri
-        @pwd_uri += lib
+        # # set pwd_uri
+        # @pwd_uri += lib
         
-        # eval lib src
-        Kernel.eval(
-            src,
-            TOPLEVEL_BINDING,
-            lib_uri.to_s
-        )
+        # # eval lib src
+        # Kernel.eval(
+        #     src,
+        #     TOPLEVEL_BINDING,
+        #     lib_uri.to_s
+        # )
 
-        # mem loaded lib
-        @loaded_uris << lib_uri.to_s
+        # # mem loaded lib
+        # @loaded_uris << lib_uri.to_s
 
-        # set this pwd_uri scope
-        @pwd_uri = last_pwd_uri
-        true
+        # # set this pwd_uri scope
+        # @pwd_uri = _pwd_uri
+        # true
     end
 
     # fetch_remote
