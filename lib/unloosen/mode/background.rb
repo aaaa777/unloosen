7c9dedc7b1dc9a0c88763a9d64f44af191a8e2c3
aaaa777(hiupc) <aaaa777@users.noreply.github.com>
2023-05-07 17:57:36 +0900

    [fix] wrong match

--- lib/unloosen/mode/background.rb
+++ lib/unloosen/mode/background.rb
@@ -3,7 +3,7 @@ require_relative "./mode"
 module Unloosen::Mode
     class Background < BaseMode
         def should_load?
-            ::Unloosen::CURRENT_EVENT != :background && super
+            ::Unloosen::CURRENT_EVENT == :background && super
         end
     end
 end
--- lib/unloosen/mode/popup.rb
+++ lib/unloosen/mode/popup.rb
@@ -3,7 +3,7 @@ require_relative "./mode"
 module Unloosen::Mode
     class Popup < BaseMode
         def should_load?
-            ::Unloosen::CURRENT_EVENT != :popup && super
+            ::Unloosen::CURRENT_EVENT == :popup && super
         end
     end
 end
