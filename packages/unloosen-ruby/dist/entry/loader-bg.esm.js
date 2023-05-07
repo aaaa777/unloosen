fa6a7524ca752e894d3749762e67cdd445bd40a7
aaaa777(hiupc) <aaaa777@users.noreply.github.com>
2023-05-07 19:14:00 +0900

    commit dist/loader-bg

--- packages/unloosen-ruby/dist/entry/loader-bg.esm.js
+++ packages/unloosen-ruby/dist/entry/loader-bg.esm.js
@@ -1,15 +1,1 @@
-importScripts('/packages/unloosen-ruby/dist/entry/init-bg.esm.js');
-
-(async() => {
-
-    // load as module
-    // https://blog.holyblue.jp/entry/2022/07/10/182137
-
-    // const path = await loadConfig("background-entry", 'packages/unloosen-ruby/dist/entry/init-bg.esm.js')
-    // const src = chrome.runtime.getURL(path);
-    // await import(src);
-})();
-main();
-
-// note: remove `Element` from dist/init-bg because Element not defined in Background
-// Element.prototype._addEventListener = Element.prototype.addEventListener;
+import '/packages/unloosen-ruby/dist/entry/init-bg.esm.js';
